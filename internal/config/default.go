5dc27cd18d2fd1897e2acc70b77aea6978240f1a
mohammadne <mohammadne.dev@gmail.com>
2023-08-20 12:14:11 +0330

    remove secrets

--- internal/config/default.go
+++ internal/config/default.go
@@ -10,12 +10,8 @@ func defaultConfig() *Config {
 		MetricsPort:    8080,
 		ProbePort:      8081,
 		LeaderElection: true,
-		Domain:         "mohammadne.me",
-		LoadbalancerIP: "86.104.38.209",
 		Cloudflare: &cloudflare.Config{
 			CloudflareURL: "https://api.cloudflare.com/client/v4",
-			Token:         "-H6dnfFIJTTu3IS97vofwuZ5TlncLN6S2mUu9PEn",
-			ZoneID:        "68cae808906f3e60c657c6c2e53db98f",
 		},
 		Logger: &logger.Config{
 			Development: true,
